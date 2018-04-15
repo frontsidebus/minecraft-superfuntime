199826259cb66e47c378f16afdad33b3a3b6f878
Paul B <paulebryant@gmail.com>
2018-04-15 17:41:22 -0500

    start_minecraft_server.sh
    Gotta start somewhere right?

---
+++ start_minecraft_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#java -Xmx4096M -Xms4096M -jar server.jar nogui
+#
+JARFILE=/home/minecraft-admin/server.jar
+#
+MAXMEMPOOL=4g
+INITMEMPOOL=1g
+#
+function start_server {
+        java -Xmx$MAXMEMPOOL -Xms$INITMEMPOOL -jar $JARFILE nogui
+}
+#
+start_server
